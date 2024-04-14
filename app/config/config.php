4f7a3ca86c5cf685ab87d05ddee89208ff2365b4
aurelioo29 <aureliolucio5@gmail.com>
2024-04-14 11:00:44 +0700

    Change URL

--- app/config/config.php
+++ app/config/config.php
@@ -1,10 +1,8 @@
-<?php 
+<?php
 
-define('BASEURL', 'http://localhost/phpMVCmalamA/public');
+define('BASEURL', 'http://localhost/MVC/public');
 
 define('DB_HOST', 'localhost');
-define('DB_USER', 'root');  
+define('DB_USER', 'root');
 define('DB_PASS', '');
 define('DB_NAME', 'phpmvc');
-
-?>
